703a6a82c5141c582af2e8eb02ff675a3b5f1e91
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2022-07-27 17:28:36 +0400

    ReflectionAPI HW Done

--- src/test/java/org/example/tests/BackendTest.java
+++ src/test/java/org/example/tests/BackendTest.java
@@ -1,13 +1,10 @@
 package org.example.tests;
 
 import org.example.annotations.BETest;
-import org.example.listeners.AnnotationsListener;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Listeners(AnnotationsListener.class)
 public class BackendTest extends BaseTest {
 
     @BETest
--- src/test/java/org/example/tests/FrontendFirstTest.java
+++ src/test/java/org/example/tests/FrontendFirstTest.java
@@ -1,13 +1,10 @@
 package org.example.tests;
 
 import org.example.annotations.UITest;
-import org.example.listeners.AnnotationsListener;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Listeners(AnnotationsListener.class)
 public class FrontendFirstTest extends BaseTest {
 
     @UITest
